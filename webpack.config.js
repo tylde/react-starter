f34ac16db912326c87a653b4fb3b5e282b38acaf
unknown <mr.mkalinowski@gmail.com>
2017-08-30 22:08:51 +0200

    commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    entry: [
+        './src/index.js'
+    ],
+    output: {
+        path: __dirname + 'dist',
+        //publicPath: '/',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            exclude: /node_modules/,
+            loader: [
+                'babel-loader'
+            ]
+        }]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+};
